18d4d40f7ee90fb88e0e45922654edf166bd2a33
KylePCT <ktugwell@live.co.uk>
2020-08-10 16:49:46 +0100

    UI implementation

--- SV_HandTrackingDemo/Assets/Scripts/Menu/pageSystem.cs
+++ SV_HandTrackingDemo/Assets/Scripts/Menu/pageSystem.cs
@@ -10,26 +10,34 @@ public class pageSystem : MonoBehaviour
     public GameObject MenuManager;
     public GameObject pageTemplate;
 
+    //struct to allow multiple customiseable serialized objects in the inspector
     [Serializable]
     public struct Pages
     {
         public int pageNumber;
         public string pageTitle;
-        [TextArea(5, 10)] public string pageInstructions;
+        [TextArea(5, 10)] public string pageInstructions; //textarea allows for bigger area to type
         public Sprite pageImage;
         public bool isComplete;
+
+        public GameObject pageTemplate;
     }
 
+    //pages list
     [SerializeField] Pages[] instructionPages;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject pageTemplate = transform.GetChild(0).gameObject;
+        Debug.Log("<b><color=white>Number of Task Instructions Found: </color></b>" + instructionPages.Length);
+
         GameObject popPages;
 
+        //for the number of tasks needed
         for (int i = 0; i < instructionPages.Length; i++)
         {
+            //instantiate the template page and populate the children with the correct information supplied
+            //from the inspector
             popPages = Instantiate(pageTemplate, transform);
             popPages.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = instructionPages[i].pageNumber.ToString();
             popPages.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = instructionPages[i].pageTitle;
@@ -37,6 +45,10 @@ public class pageSystem : MonoBehaviour
             popPages.transform.GetChild(3).GetComponent<Image>().sprite = instructionPages[i].pageImage;
 
             popPages.transform.SetParent(MenuManager.transform.parent, false);
+            popPages.SetActive(false);
         }
+
+        //need to figure out how to hook up the UI buttons to work with each of these pages
+        //perhaps could instantiate them also and do i+1 and i-1 for the page numbers with a check for 0 and .length for min and max
     }
 }
