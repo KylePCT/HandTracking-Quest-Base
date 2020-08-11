ed0490a1c710471fb78093098406a98a992ca7f7
KylePCT <ktugwell@live.co.uk>
2020-08-11 14:52:30 +0100

    Push/Pull demo

--- SV_HandTrackingDemo/Assets/Scripts/HingeJointListener.cs
+++ SV_HandTrackingDemo/Assets/Scripts/HingeJointListener.cs
@@ -3,6 +3,8 @@ using UnityEngine.Events;
 
 public class HingeJointListener : MonoBehaviour
 {
+    public bool isHingeJoint;
+
     //angle threshold to trigger if we reached limit
     public float angleBetweenThreshold = 1f;
 
@@ -14,63 +16,40 @@ public class HingeJointListener : MonoBehaviour
     //Event called on max reached
     public UnityEvent OnMaxLimitReached;
 
-    public bool isHingeJoint;
-    public bool isConfigurableJoint;
-
     public enum HingeJointState { Min, Max, None }
     private HingeJoint hinge;
-
-    public enum ConfigJointState { Min, Max, None }
-    private ConfigurableJoint configHinge;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (isHingeJoint)
-        {
-            hinge = GetComponent<HingeJoint>();
-        }
-
-        if (isConfigurableJoint)
-        {
-            configHinge = GetComponent<ConfigurableJoint>();
-        }
+        hinge = GetComponent<HingeJoint>();
     }
 
     private void FixedUpdate()
     {
+        float angleWithMinLimit = Mathf.Abs(hinge.angle - hinge.limits.min);
+        float angleWithMaxLimit = Mathf.Abs(hinge.angle - hinge.limits.max);
 
-        if (isHingeJoint)
+        //Reached Min
+        if (angleWithMinLimit < angleBetweenThreshold)
         {
-            float angleWithMinLimit = Mathf.Abs(hinge.angle - hinge.limits.min);
-            float angleWithMaxLimit = Mathf.Abs(hinge.angle - hinge.limits.max);
+            if (hingeJointState != HingeJointState.Min)
+                OnMinLimitReached.Invoke();
 
-            //Reached Min
-            if (angleWithMinLimit < angleBetweenThreshold)
-            {
-                if (hingeJointState != HingeJointState.Min)
-                    OnMinLimitReached.Invoke();
-
-                hingeJointState = HingeJointState.Min;
-            }
-            //Reached Max
-            else if (angleWithMaxLimit < angleBetweenThreshold)
-            {
-                if (hingeJointState != HingeJointState.Max)
-                    OnMaxLimitReached.Invoke();
-
-                hingeJointState = HingeJointState.Max;
-            }
-            //No Limit reached
-            else
-            {
-                hingeJointState = HingeJointState.None;
-            }
+            hingeJointState = HingeJointState.Min;
         }
-
-        if (isConfigurableJoint)
+        //Reached Max
+        else if (angleWithMaxLimit < angleBetweenThreshold)
         {
+            if (hingeJointState != HingeJointState.Max)
+                OnMaxLimitReached.Invoke();
 
+            hingeJointState = HingeJointState.Max;
+        }
+        //No Limit reached
+        else
+        {
+            hingeJointState = HingeJointState.None;
         }
     }
 }
---
+++ SV_HandTrackingDemo/Assets/Scripts/LinearLimitListener.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LinearLimitListener : MonoBehaviour
+{
+    public GameObject ConfigJoint_Obj;
+
+    private Vector3 initialPos;
+    private Vector3 endPos;
+    private float linearLimit;
+
+    //Event called on min reached
+    public UnityEvent OnMinLimitReached;
+    //Event called on max reached
+    public UnityEvent OnMaxLimitReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = ConfigJoint_Obj.transform.position;
+
+        linearLimit = ConfigJoint_Obj.GetComponent<ConfigurableJoint>().linearLimit.limit;
+
+        endPos = ConfigJoint_Obj.transform.position;
+        endPos.y = ConfigJoint_Obj.transform.position.y + linearLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(initialPos + "" + endPos + "" + transform.position);
+
+        if (ConfigJoint_Obj.transform.position.x <= initialPos.x)
+        {
+            OnMinLimitReached.Invoke();
+        }
+
+        else if (ConfigJoint_Obj.transform.position.x > initialPos.x)
+        {
+            OnMaxLimitReached.Invoke();
+        }
+
+        else
+        {
+            return;
+        }
+    }
+}
